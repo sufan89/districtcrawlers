ca473e3aad86244bb3593701e94d48a8ed40dfc0
sufan <sufan_89@hotmail.com>
2019-03-01 19:18:39 +0800

    代码提交

--- src/config/main_config.py
+++ src/config/main_config.py
@@ -7,4 +7,6 @@ class config:
     '''目标URL'''
     targetUrl = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/'
     '''结果存储位置'''
-    saveFloder = '/home/sufan/Desktop/data/dicdata'
+    saveFloder = '/home/sufan/Desktop/data/dicdata/'
+    '''样式表'''
+    classType = {0: 'provincetr', 1: 'citytr', 2: 'countytr', 3: 'towntr', 4: 'villagetr'}
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 import chardet
 import config
+import os
 
 
 def connToUrl(url):
@@ -29,7 +30,7 @@ def GetDistrictData():
         print('获取数据失败')
         return
     fullDistrict = {}
-    for i in bs.find_all('tr', 'provincetr'):
+    for i in bs.find_all('tr', config.config.classType[0]):
         '''循环获取省信息'''
         for j in i.find_all('td'):
             provinceName = j.getText()
@@ -37,17 +38,47 @@ def GetDistrictData():
             if elmenta is not None:
                 hrefstr = elmenta.get('href')
             fullDistrict[provinceName] = hrefstr
-    print(fullDistrict)
+    DowLoadData(fullDistrict)
 
 
 def DowLoadData(dicProvinces):
     '''下载数据'''
-    pass
+    if len(dicProvinces) == 0:
+        print('没有要下载的数据')
+    for key in dicProvinces.keys():
+        '''创建文件'''
+        filename = config.config.saveFloder + key + '.csv'
+        if not CreateFile(filename):
+            continue
+        urlstr = config.config.targetUrl + dicProvinces[key]
+        fs = os.open(filename, os.O_WRONLY)
+        print('创建文件：%s.csv 成功' % key)
+        GetUrlData(urlstr, 1, fs, [dicProvinces[key].split('.')[0], key])
+
+
+def GetUrlData(url, classtype, filestream, rowdata):
+    '''循环请求网页，并将数据存入到文件'''
+    bs = connToUrl(url)
+    if bs is None:
+        print('请求地址失败:', url)
+    if classtype == 4:
+        '''村级，写数据到文件'''
+    else:
+        '''乡镇以上级，继续请求'''
+        print(rowdata)
+        GetUrlData(url, classtype + 1, filestream, rowdata)
 
 
 def CreateFile(fileName):
     '''创建文件'''
-    pass
+    if os.path.exists(fileName):
+        os.remove(fileName)
+    try:
+        os.mknod(fileName)
+        return True
+    except:
+        print('创建文件：%s 失败！' % fileName)
+        return False
 
 
 def writeDataToFile(rowDt):
